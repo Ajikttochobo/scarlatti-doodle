af72d9bd0b0ef0548d39dabeadc735fb82761254
Ajikttochobo <139213349+Ajikttochobo@users.noreply.github.com>
2026-08-15 15:28:40 +0000

    working on gradio interface

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
 import gradio as gr
+import ScarlattiMelodyModel
 
-def dummyFuc():
-    return "hello"
+# ********gradio 함수들********
+
+def onMidiFileChange(inputMidiFile):
+    if(inputMidiFile == None):
+        return gr.update(visible=False), gr.update(visible=False)
+    else:
+        return gr.update(visible=True), gr.update(visible=True)
+
+#********gradio 인터페이스 코드********
 
 with gr.Blocks(title="Scarlatti Doodle") as demo:
-    gr.Markdwon("hello")
+    gr.Markdown("# Scarlatti Doodle")
+    gr.Markdown("Google Bach doodle but it's Scarlatti")
+
+    gr.Markdown("---")
+
+    with gr.Row():
+
+        with gr.Column():
+            gr.Markdown("column1")
+            input_midi_file = gr.File(file_types= [".midi", ".mid"], label="upload midi file")
+
+            pianoroll_plot = gr.Plot(visible=False)
+            play_button = gr.Button(value="▶️ play", visible=False)
+            
+            with gr.Accordion(label="input midi settings",open=False):
+                gr.Markdown("accordion!")
+
+        with gr.Column():
+            gr.Markdown("column2")
+
+    # ********ui 함수들********
+
+    input_midi_file.change(fn=onMidiFileChange, 
+                           inputs=input_midi_file,
+                           outputs= [play_button, pianoroll_plot])
+
+
 
 if __name__ == "__main__":
     demo.launch()
