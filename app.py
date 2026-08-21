ea8b28c294a955bf7169a60a9e0a6c9ce030f92e
Ajikttochobo <139213349+Ajikttochobo@users.noreply.github.com>
2026-08-21 08:17:44 +0000

    final fr

--- app.py
+++ app.py
@@ -116,7 +116,5 @@ with gr.Blocks(title="Scarlatti Doodle") as demo:
                            inputs = None,
                            outputs = [input_midi_file, sampleFileButton, sampleMarkdown])
 
-
-
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860)
